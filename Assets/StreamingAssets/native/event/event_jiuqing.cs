dc680e098c897b3c236e49bef0f826d9da492a0e
sericaer <sericaer@outlook.com>
2018-05-14 18:07:21 +0800

    add event

--- Assets/StreamingAssets/native/event/event_jiuqing.cs
+++ Assets/StreamingAssets/native/event/event_jiuqing.cs
@@ -172,4 +172,43 @@ namespace native
             return prob;
         }
     }
+
+    class EVENT_SG_EMPTY : EVENT_HD
+    {
+        bool Precondition()
+        {
+            emptyOffice = GMData.GetOffice(Selector.ByOffice("SGX").ByPerson(null));
+            if (emptyOffice != null)
+            {
+                Person[] persons = GMData.GetPersons(Selector.ByOffice("JQX")));
+
+                persons.Where();
+
+                return true;
+            }
+                
+            return false;         
+        }
+
+        class OPTION1 : Option
+        {
+            void Selected(ref string nxtEvent, ref string param)
+            {
+                GMData.Emp.Heath--;
+            }
+        }
+
+        float CalcProb()
+        {
+            float prob = 0.001f;
+            if (GMData.TianWenStatus.Contains("STATUS_YHSX"))
+            {
+                prob = 0.05f;
+            }
+
+            return prob;
+        }
+
+        private Office emptyOffice;
+    }
 }
