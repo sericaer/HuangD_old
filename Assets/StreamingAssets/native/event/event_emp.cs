2596e4bce3402ced925f8e06c82a382426198cca
sericaer <sericaer@outlook.com>
2018-05-14 22:31:51 +0800

    拆分事件到不同的文件

---
+++ Assets/StreamingAssets/native/event/event_emp.cs
@@ -0,0 +1,37 @@
+﻿using HuangDAPI;
+using System.Linq;
+
+namespace native
+{
+    class EVENT_EMP_HEATH_DEC : EVENT_HD
+    {
+        bool Precondition()
+        {
+            float prob = CalcProb();
+
+            if (Probability.IsProbOccur(prob))
+                return true;
+
+            return false;
+        }
+
+        class OPTION1 : Option
+        {
+            void Selected(ref string nxtEvent, ref string param)
+            {
+                GMData.Emp.Heath--;
+            }
+        }
+
+        float CalcProb()
+        {
+            float prob = 0.001f;
+            if (GMData.TianWenStatus.Contains("STATUS_YHSX"))
+            {
+                prob = 0.05f;
+            }
+
+            return prob;
+        }
+    }
+}
---
+++ Assets/StreamingAssets/native/event/event_person_common.cs
@@ -0,0 +1,72 @@
+﻿using HuangDAPI;
+using System.Linq;
+
+namespace native
+{
+    class EVENT_PERSON_SUICDIE : EVENT_HD
+    {
+        bool Precondition()
+        {
+            Person[] persons = GMData.GetPersons();
+            foreach(Person p in persons)
+            {
+                float prob = CalcProb(p.press);
+                if (Probability.IsProbOccur(prob))
+                {
+                    currPerson = p;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        string Desc()
+        {
+            return UI.Format("EVENT_PERSON_SUICDIE_DESC", currPerson.ToString());
+        }
+
+        class OPTION1 : Option
+        {
+            void Selected(ref string nxtEvent, ref string param)
+            {
+                Office office = GMData.GetOffice(Selector.ByPerson(OUTTER.currPerson.name));
+                OUTTER.currPerson.Die();
+
+                if (office.name == "SG1" || office.name == "SG2" || office.name == "SG3")
+                {
+                    nxtEvent = "EVENT_STAB_DEC";
+                }
+            }
+
+            EVENT_PERSON_SUICDIE OUTTER;
+        }
+
+        float CalcProb(int press)
+        {
+            switch(press)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    return 0.0f;
+                case 5:
+                case 6:
+                    return 0.03f;
+                case 7:
+                    return 0.05f;
+                case 8:
+                    return 0.08f;
+                case 9:
+                    return 0.2f;
+                case 10:
+                    return 0.4f;
+                default:
+                    return 0.0f;
+            }
+        }
+
+        private Person currPerson;
+    }
+}
